ab6dafc62aa3ca0be67074173514ae729363722d
Nazir93 <118357281+Nazir93@users.noreply.github.com>
2023-07-29 03:43:19 +0300

    Add files via upload

---
+++ src/sections/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { FaRegStar } from "react-icons/fa";
+import { FiGitBranch } from "react-icons/fi";
+
+function Footer() {
+  const [githubInfo, setGitHubInfo] = useState({
+    stars: null,
+    forks: null,
+  });
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/koolkishan/chat-app-react-nodejs")
+      .then((response) => response.json())
+      .then((json) => {
+        const { stargazers_count, forks_count } = json;
+        setGitHubInfo({
+          stars: stargazers_count,
+          forks: forks_count,
+        });
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  return (
+    <footer>
+     <span className="footer-info">Designed and Built by Lali Gochadze.</span>
+    </footer>
+  );
+}
+
+export default Footer;
